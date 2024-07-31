8b63597f0e9bacecb4416de67d0a2a801f7c6a6b
Louis Daily <developer@louisdaily.com>
2024-07-31 15:49:20 -0500

    refactor: Rename all instances to DailyHabits

--- DailyHabits/DailyHabitsTests/DailyHabitsTests.swift
+++ DailyHabits/DailyHabitsTests/DailyHabitsTests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import Daily_Habits
+@testable import DailyHabits
 
-final class Daily_HabitsTests: XCTestCase {
+final class DailyHabitsTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
